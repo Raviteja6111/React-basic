1afc6744cb47e2c1efed5b3cba5630e4c6edc9e3
Raviteja6111 <tejar6111@gmail.com>
2023-08-17 17:59:36 +0530

    today update

--- App.js
+++ App.js
@@ -1,16 +1,56 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
-const h1tag = React.createElement("div",{ id : "parent"},
-React.createElement("div",{ id : "child"}),
-React.createElement("h1",{ id : "some"}, "Hello all , i am from react js")
-);
+//React create element 
+const h1tag = React.createElement("div",{ id : "parent"},"some text");
 
+//JSX 
+//Babel - 
+
+let name = "Teja";
+let mobile  = "9666119069"
+const jsxdivision = <div id="myid" className="my-div">my name is {name} my mobile number is {mobile}</div>
+
+//React Components
+
+//Everything in React components. 2 ways) 
+//1) Class based component (old way)
+//2)Function/methods based components ()
+//....................Component...................
+const userDetailes = {
+    username: "Raviteja",
+    mobile: "9666119069"
+
+}
+
+const ButtonCOM = () => (
+    <input type="text"/>
+);
+const HeaderComponent = () => (
+    <div class="header"><h2>my Header</h2><ButtonCOM/></div>
+);
+const ContentComponent = () => (
+    <div class="content">
+        <h2>The user name: { userDetailes.username }</h2>
+        </div>
+);
+const FooterComponent = () => (
+    <div class="footer"><h2>my footer</h2></div>
+)
+
+const HomepageComponent = () => (
+    <div className="container">
+    <HeaderComponent></HeaderComponent>,
+    <ContentComponent></ContentComponent>,
+    <FooterComponent></FooterComponent>
+
+    </div>
+)
 
 console.log(h1tag);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(h1tag);
+root.render(<HomepageComponent/>); //Reactjs element - HTML-Element 
 
 
--- README.md
+++ README.md
@@ -4,10 +4,20 @@
 # npm install
 # npx parcel index.html
 
+# to start the project do run bellow commands
+npm install
+npm run start
+npm run build
 # parcel
 
 - Dev - Build.
 - Local Server
 - HMR - HOT module replacement
 - cache
-- 
+- image optimaization
+- minifction
+- code spliting
+- error handling
+- Hosting on HTTPS
+- difrent biudels like Dev and PROD
+
