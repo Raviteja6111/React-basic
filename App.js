46b82b702558827c9f072984c45e8c5967ab0181
Raviteja6111 <tejar6111@gmail.com>
2023-08-18 09:47:54 +0530

    props

--- App.js
+++ App.js
@@ -1,56 +1,85 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
-//React create element 
-const h1tag = React.createElement("div",{ id : "parent"},"some text");
+/**
+ * HeaderComponent
+ *  -LOGO
+ *  -Menu Item
+ * 
+ * BodyComponent
+ *  -SerchBar
+ *  RestoComponent
+ *   -Image
+ *   -RestoName
+ *   -Rating
+ *   -deliveryTime
+ * 
+ * FooterComponent
+ * copyrights
+ * links
+ */
 
-//JSX 
-//Babel - 
-
-let name = "Teja";
-let mobile  = "9666119069"
-const jsxdivision = <div id="myid" className="my-div">my name is {name} my mobile number is {mobile}</div>
-
-//React Components
-
-//Everything in React components. 2 ways) 
-//1) Class based component (old way)
-//2)Function/methods based components ()
-//....................Component...................
-const userDetailes = {
-    username: "Raviteja",
-    mobile: "9666119069"
-
-}
-
-const ButtonCOM = () => (
-    <input type="text"/>
-);
-const HeaderComponent = () => (
-    <div class="header"><h2>my Header</h2><ButtonCOM/></div>
-);
-const ContentComponent = () => (
-    <div class="content">
-        <h2>The user name: { userDetailes.username }</h2>
+const HeaderComponent = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="https://www.google.com/search?q=food+logo&oq=food+lo&gs_lcrp=EgZjaHJvbWUqCggBEAAYsQMYgAQyBggAEEUYOTIKCAEQABixAxiABDIHCAIQABiABDIKCAMQABixAxiABDIHCAQQABiABDIVCAUQLhgKGIMBGMcBGLEDGNEDGIoFMgcIBhAAGIAEMgcIBxAAGIAEMgcICBAAGIAEMgcICRAAGIAE0gEIODU4MWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8#vhid=q2Xp52tK05j7UM&vssid=l" alt=""></img>
+            </div>
+            <div className="navitem">
+            <ul>
+                <li>Home</li>
+                <li>AboutUs</li>
+                <li>Service</li>
+                <li>Top offers</li>
+                <li>my Card</li>
+            </ul>
+            </div>
         </div>
-);
-const FooterComponent = () => (
-    <div class="footer"><h2>my footer</h2></div>
-)
+    )
+    };
 
-const HomepageComponent = () => (
-    <div className="container">
-    <HeaderComponent></HeaderComponent>,
-    <ContentComponent></ContentComponent>,
-    <FooterComponent></FooterComponent>
+    const RestoComponent = () =>{
+        return(
+            <div className="resto">
+                <img src="https://www.google.com/search?q=tiffin+images&tbm=isch&ved=2ahUKEwjf_ciupOWAAxV3k2MGHVYDAzgQ2-cCegQIABAA&oq=tiffin+&gs_lcp=CgNpbWcQARgEMgoIABCKBRCxAxBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgUIABCABDIFCAAQgAQyBwgAEIoFEEM6BAgjECc6CAgAEIAEELEDULAPWLAPYOgaaABwAHgAgAG-AYgB7AKSAQMwLjKYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=4-feZJ-AC_emjuMP1oaMwAM&bih=825&biw=598#imgrc=xDkVGOli5jnfeM"/>
+                <h2>bablu tifins</h2>
+                <p>Biryani, veg</p>
+                <h4>4.5</h4>
+                <h4>In 40 mins</h4>
+            </div>
+        )
+    };
 
-    </div>
-)
+const BodyComponent = () =>{
+    return (
+        <div className="body">
+            <div className="resto-container">
+            <RestoComponent />
+            <RestoComponent/>
+            <RestoComponent/>
+            <RestoComponent/>
+            
+            </div>
+        </div>
+    )
+    };
 
-console.log(h1tag);
+    const FooterComponent = () =>{
+        return(
+            <div className="footer"><h2>FooterComponent</h2></div>
+        )
+    }
+
+    const AppLayout = () =>{
+        return (
+            <div className="container-fluid">
+                <HeaderComponent></HeaderComponent>
+                <BodyComponent></BodyComponent>
+                <FooterComponent></FooterComponent>
+            </div>
+
+        )
+    }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<HomepageComponent/>); //Reactjs element - HTML-Element 
-
-
+root.render(<AppLayout></AppLayout>);
