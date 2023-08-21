634767bd647b99b539529360fbef923956af1ce7
Raviteja6111 <tejar6111@gmail.com>
2023-08-21 14:43:40 +0530

    props-multicomponents

--- App.js
+++ App.js
@@ -17,13 +17,14 @@ import ReactDOM  from "react-dom";
  * FooterComponent
  * copyrights
  * links
+ * properties for components
  */
 
 const HeaderComponent = () => {
     return (
         <div className="header">
             <div className="logo">
-                <img src="https://www.google.com/search?q=food+logo&oq=food+lo&gs_lcrp=EgZjaHJvbWUqCggBEAAYsQMYgAQyBggAEEUYOTIKCAEQABixAxiABDIHCAIQABiABDIKCAMQABixAxiABDIHCAQQABiABDIVCAUQLhgKGIMBGMcBGLEDGNEDGIoFMgcIBhAAGIAEMgcIBxAAGIAEMgcICBAAGIAEMgcICRAAGIAE0gEIODU4MWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8#vhid=q2Xp52tK05j7UM&vssid=l" alt=""></img>
+                <img src="https://img.freepik.com/premium-vector/good-food-logo-template_79169-17.jpg?w=996" alt=""></img>
             </div>
             <div className="navitem">
             <ul>
@@ -38,14 +39,15 @@ const HeaderComponent = () => {
     )
     };
 
-    const RestoComponent = () =>{
+    const RestoComponent = (props) =>{
+        console.log(props);
         return(
             <div className="resto">
-                <img src="https://www.google.com/search?q=tiffin+images&tbm=isch&ved=2ahUKEwjf_ciupOWAAxV3k2MGHVYDAzgQ2-cCegQIABAA&oq=tiffin+&gs_lcp=CgNpbWcQARgEMgoIABCKBRCxAxBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgUIABCABDIFCAAQgAQyBwgAEIoFEEM6BAgjECc6CAgAEIAEELEDULAPWLAPYOgaaABwAHgAgAG-AYgB7AKSAQMwLjKYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=4-feZJ-AC_emjuMP1oaMwAM&bih=825&biw=598#imgrc=xDkVGOli5jnfeM"/>
-                <h2>bablu tifins</h2>
-                <p>Biryani, veg</p>
-                <h4>4.5</h4>
-                <h4>In 40 mins</h4>
+                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/8523266de1213e8ea57e5df4eedf747e"></img>
+                <h2>{props.resName}</h2>
+                <p>{props.resRating}</p>
+                <h4>{props.resDeliverytime}</h4>
+                <h4>{props.resMake}</h4>
             </div>
         )
     };
@@ -54,10 +56,26 @@ const BodyComponent = () =>{
     return (
         <div className="body">
             <div className="resto-container">
-            <RestoComponent />
-            <RestoComponent/>
-            <RestoComponent/>
-            <RestoComponent/>
+            <RestoComponent 
+            resName = "Bablu" 
+            resRating = "3.6" 
+            resDeliverytime="30min" 
+            resMake="Biryani, Tandoori" />
+            <RestoComponent
+               resName = "KFC - food" 
+               resRating = "3.9" 
+               resDeliverytime="20min" 
+               resMake="Fried pices"/>
+            <RestoComponent
+               resName = "sravanthi-food" 
+               resRating = "5" 
+               resDeliverytime="25min" 
+               resMake="All indian food"/>
+            <RestoComponent
+               resName = "keerthi" 
+               resRating = "2.5" 
+               resDeliverytime="30min" 
+               resMake="Biryani, Tandoori"/>
             
             </div>
         </div>
